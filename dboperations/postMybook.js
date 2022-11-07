ede411df1a3d16ed379b4f63bac4a6d5e21e8f1f
hi-ozdemir <halilibo010100@gmail.com>
2022-11-07 23:10:01 +0300

    create get user book query

---
+++ dboperations/postMybook.js
@@ -0,0 +1,22 @@
+var config = require("../dbconfig");
+const sql = require("mssql");
+const register = require("./register");
+
+async function postMyBook(data) {
+  try {
+    let pool = await sql.connect(config);
+    let mybook = await pool
+      .request()
+      .input("username", sql.VarChar, data.username)
+      .input("password", sql.VarChar, data.password)
+      .input("request", sql.VarChar, data.request)
+      .query("spMybooks @username , @password, @request");
+    return mybook.recordsets;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = {
+  postMyBook: postMyBook,
+};
